1d18b32da2743a665dcaf5027a27eb052715f13d
drewpearce <drew@caffdev.com>
2019-05-07 22:36:31 -0400

    Specs (#8)
    * update gitignore
    
    * gitignore correction
    
    * specs python solution 1
    
    * add notes
    
    * Closer to fix
    
    * Removed masking function and change print outs
    
    * Remove print out and clean up
    
    * python-dnesbitt03qub: Change lat test to be more efficient & add basic profiling
    
    * Fix error in Dockerfile
    
    * Add cython test
    
    * Correct output to cython
    
    * specs refactor

--- python-specs/test.py
+++ python-specs/test.py
@@ -6,9 +6,34 @@ LANG = 'python'
 NOTES = 'ugly. needs cleanup.'
 PATH = os.path.join(
     os.path.abspath(os.path.dirname(__file__)),
+    '..',
     'data',
     'all.csv'
 )
+
+
+def load_data():
+    with open(PATH) as f:
+        temp = [
+            l for l in f.readlines()
+            if not l.startswith('StnID')
+            and ',' in l
+            and float(l[12:19]) > 45.0000
+        ]
+
+    return temp
+
+
+def transform_data(temp):
+    data = {}
+    for row in temp[1:]:
+        x = row.split(',')
+        if x[1] == 'Lat' or float(x[1]) > 45.0:
+            if x[0] not in data:
+                data[x[0]] = 0
+            data[x[0]] += sum(1 for t in x[6::5] if float(t) > 0.0)
+
+    return data
 
 
 def print_results(result, runtime):
@@ -16,18 +41,8 @@ def print_results(result, runtime):
 
 
 start = int(round(time.time() * 1000))
-data = {}
-with open(PATH) as f:
-    temp = f.readlines()
-
-for row in temp[1:]:
-    if ',' in row:
-        x = row.split(',')
-        if x[1] == 'Lat' or float(x[1]) > 45.0:
-            if x[0] not in data:
-                data[x[0]] = 0
-            data[x[0]] += sum(1 for t in x[6::5] if float(t) > 0.0)
-
+temp = load_data()
+data = transform_data(temp)
 data = sorted(
     [
         {
@@ -39,6 +54,5 @@ data = sorted(
     key=lambda k: k['total'],
     reverse=True
 )
-
 end = int(round(time.time() * 1000)) - start
 print_results(data[0]['station'][3:], end)
