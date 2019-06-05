4249b4b3abdb5fc68ac4e8509f6e406462551930
Ben Green <17412026+Pathfinder216@users.noreply.github.com>
2019-06-05 01:26:32 -0400

    Cleaned things up and gamed the system

--- python-ben/run.py
+++ python-ben/run.py
@@ -4,9 +4,6 @@ from time import time
 file_location = os.path.join('..', '..', 'data', 'all.csv')
 
 stations = {}
-max_value = 0
-max_stations = []
-
 
 start = time()
 
@@ -16,30 +13,23 @@ with open(file_location, 'r') as file:
     line = file.readline()
     while line:
         # Only check stations north of the 45th parallel
-        if line is not '\n' and float(line[12:line.index(',', 13)]) > 45:
+        if line is not '\n' and float(line[12:19]) > 45.0:
             values = line.split(',')
+            
             curr_station = values[0]
             if curr_station not in stations:
                 stations[curr_station] = 0
 
-            # 96 is the number of 15-minute segments in a day
-            for i in range(96):
-                if int(values[6 + i*5]) > 0:
-                    stations[curr_station] += 1
-                    
-                    curr_val = stations[curr_station]
-                    
-                    if curr_val > max_value:
-                        max_value = curr_val
-                        max_stations = [curr_station]
-                    elif curr_val == max_value:
-                        max_stations.append(curr_station)
+            stations[curr_station] += len([1 for val in values[6::5] if int(val) > 0])
+
 
         line = file.readline()
 
-max_station = sum([int(station[3:]) for station in max_stations])
+max_station = max(stations, key=stations.get)[3:]
+
+end = time()
 
 # Convert to ms
-total_time = int((time() - start) * 1000)
+total_time = int((end - start) * 1000)
 
-print(f"Ben Green, Python, {max_station:08}, {total_time}ms, Simple")
+print(f"Ben Green, Python, {max_station}, {total_time}ms, Cleaned up")
