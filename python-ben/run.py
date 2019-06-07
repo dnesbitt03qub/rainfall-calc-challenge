3745033cfcff359bc30c78131d2e8febc0c75dcf
Ben Green <17412026+Pathfinder216@users.noreply.github.com>
2019-06-06 21:58:01 -0400

    Remove max_ variable usage

--- python-ben/run.py
+++ python-ben/run.py
@@ -38,8 +38,6 @@ def assume_single_max():
 
 def possible_multiple_max():
     stations = defaultdict(int)
-    max_value = 0
-    max_stations = []
     start = time()
 
     with open(file_location, 'r') as file:
@@ -62,26 +60,15 @@ def possible_multiple_max():
                 
                 values = line.split(',')
                 
-                curr_station = values[0]
+                #curr_station = values[0]
 
                 # 96 is the number of 15-minute segments in a day
-                for i in range(96):
-                    start_char = values[6 + i*5][0]
-                    if start_char is not '-' and start_char is not '0':
-                        stations[curr_station] += 1
-                        
-                        curr_val = stations[curr_station]
-                        
-                        if curr_val > max_value:
-                            max_value = curr_val
-                            max_stations = [curr_station]
-                        elif curr_val == max_value:
-                            max_stations.append(curr_station)
-
+                stations[values[0]] += len([1 for val in values[6::5] if val[0] not in ('-', '0')])
 
             line = file.readline()
 
-    max_station = sum([int(station[3:]) for station in max_stations])
+    max_value = max(stations.values())
+    max_station = sum([int(station[3:]) for station in stations if stations[station] == max_value])
     end = time()
     return max_station, int((end - start) * 1000)
 
