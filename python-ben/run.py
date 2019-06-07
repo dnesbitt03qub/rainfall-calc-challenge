3e67b6de93feb36f31449ad8348ffbc9158a767d
Ben Green <17412026+Pathfinder216@users.noreply.github.com>
2019-06-06 20:59:03 -0400

    Add ascii value check optimization

--- python-ben/run.py
+++ python-ben/run.py
@@ -12,8 +12,19 @@ with open(file_location, 'r') as file:
     # Ignore the headers
     line = file.readline()
     while line:
-        # Only check stations north of the 45th parallel
-        if line is not '\n' and float(line[12:19]) > 45.0:
+        # Ensure line isn't empty and station is north of the 45th parallel
+        if line is not '\n' and (
+            # At least the 50th parallel
+            ord(line[12]) > 52 or (
+                # 40-49.9999 latitude
+                ord(line[12]) == 52 and (
+                    # At least the 46th parallel
+                    ord(line[13]) > 53 or (
+                        # 45-45.9999
+                        ord(line[13]) == 53 and (
+                            # Not exactly the 45th parallel
+                            line[15] is not '0' or line[16] is not '0' or line[17] is not '0' or line[18] is not '0'))))):
+            
             values = line.split(',')
             
             curr_station = values[0]
